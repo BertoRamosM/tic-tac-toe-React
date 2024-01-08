35df2f6d4c3f74ecd6c85d3500d3736968603842
ARamos <albertramos902@gmail.com>
2024-01-08 10:48:16 +0100

    localstorage added

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,17 @@ import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) :
+      Array(9).fill(null);
+})
 
-  const [turn, setTurn] = useState(TURNS.X);
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X;
+  })
 
   const [winner, setWinner] = useState(null);
 
@@ -21,6 +29,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   };
 
  
@@ -34,11 +45,17 @@ function App() {
     //in the index store the value of turn x / o
     newBoard[index] = turn;
     //then set the board with the new manipulated board
-    setBoard(newBoard);
+    setBoard(newBoard); 
     //if turn is x change to o or viceversa
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     //change the turn state
     setTurn(newTurn);
+
+    //save game
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
+    //check if winner
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
       confetti()
